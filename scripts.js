7aa3324c17394adcb69ecd98daadd6c1e9e201db
choski91 <mlaura.smichowski@gmail.com>
2025-06-14 21:27:52 +0200

    hasta el ejercicio 3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log(document.title);
+//ejercicio 1
+document.getElementById("gen-1").innerHTML = "Generasión 1 Pokimon";
+
+//ejercicio 2
+document.querySelector(".infocard-list.infocard-list-pkmn-lg").style.backgroundColor = "green";
+
+//ejercicio 3
+console.log(document.querySelector('link[rel="canonical"]').href);
+
+
