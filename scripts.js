30067d8efe4ac63309751b3912b18caa87aac33d
choski91 <mlaura.smichowski@gmail.com>
2025-06-15 20:29:20 +0200

    hasta el ejercicio 6

--- scripts.js
+++ scripts.js
@@ -7,4 +7,28 @@ document.querySelector(".infocard-list.infocard-list-pkmn-lg").style.backgroundC
 //ejercicio 3
 console.log(document.querySelector('link[rel="canonical"]').href);
 
+//ejercicio 4
 
+let url = document.querySelector('link[rel="canonical"]').href;
+
+let dominio = url.slice(8, 21);
+
+console.log(dominio); 
+
+//ejercicio 5
+
+let todasLasImg = document.querySelectorAll(".img-fixed.img-sprite");
+
+for( let i = 0; i < todasLasImg.length; i++) {
+  
+    console.log(todasLasImg[i]);
+}
+
+//ejercicio 6
+
+let allImg = document.querySelectorAll(".img-fixed.img-sprite");
+
+for( let i = 0; i < allImg.length; i++) {
+  
+   allImg[i].srcset= "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}
